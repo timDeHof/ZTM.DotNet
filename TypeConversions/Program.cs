17e716062056e8ec8622f7828f1bf0b7e01e28f7
timDeHof <ttdehof@gmail.com>
2023-06-04 09:30:09 -0400

    Add files via upload

---
+++ TypeConversions/Program.cs
@@ -0,0 +1,19 @@
+﻿// Implicit conversions: No special syntax, no data loss, no precision loss
+// Explicit conversions: Castings, potential data loss, potential precision loss
+
+int age = 31;
+float weight = 78.7f;
+
+Console.WriteLine($"age: {age}, weight: {weight}");
+
+// implicit conversions
+long myAge = age;
+
+// explicit conversions
+int lessWeight = (int)weight; // precision loss
+int notMyAge = (int)myAge; // no precision loss
+
+long bigNumber = 1111111111111111111;
+int smallerType = (int)bigNumber;
+
+Console.WriteLine(smallerType);
